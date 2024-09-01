422c7d53ce6f39c6dd1f690c32482273d28576a4
Serzh070501 <serzh.nahapetyan2001@gmail.com>
2024-09-01 19:18:18 +0400

    My first git commit

---
+++ Hayk/src/Lesson1.java
@@ -0,0 +1,44 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Lav txa");
+        System.out.println("Lennakanci");
+        byte x = 98;
+        System.out.println(x);
+        short y = 7635;
+        System.out.println(y);
+        int k = -2616213;
+        System.out.println(k);
+        long a = -1232176442L;
+        System.out.println(a);
+        float l = 66.1f;
+        System.out.println(l);
+        double s = 44.4;
+        System.out.println(s);
+        boolean m = true;
+        System.out.println(m);
+        m = false;
+        System.out.println(m);
+        x = 8;
+        System.out.println(x);
+        char w = 'x';
+        System.out.println(w);
+        char ff = '\u054A';
+        System.out.println(ff);
+        String g = "Hayk du krnas";
+        System.out.println(g);
+        String Anun = "Hayk";
+        String azganun = "Harutyunyan";
+        System.out.println(Anun + " " + azganun);
+        int b = 222;
+        System.out.println("b= " + b);
+
+        String gg = "Gogor du lav txa ches";
+        System.out.println(gg);
+
+        String ee = "isk es lav txa em";
+        System.out.println(ee);
+
+    }
+
+
+}
